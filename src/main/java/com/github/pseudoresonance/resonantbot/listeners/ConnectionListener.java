1a6866a66d96f05b39e2dd279c7a68283ee8de48
PseudoResonance <kaio11602@gmail.com>
2018-08-25 15:27:47 -0700

    Updated JDA to 3.7.1_385
    Fixed bot status disappearing after reconnect

--- src/main/java/com/github/pseudoresonance/resonantbot/listeners/ConnectionListener.java
+++ src/main/java/com/github/pseudoresonance/resonantbot/listeners/ConnectionListener.java
@@ -1,10 +1,17 @@
 package com.github.pseudoresonance.resonantbot.listeners;
 
 import com.github.pseudoresonance.resonantbot.Config;
+
+import net.dv8tion.jda.core.events.ReconnectedEvent;
 import net.dv8tion.jda.core.events.ResumedEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
 
 public class ConnectionListener extends ListenerAdapter {
+
+	@Override
+	public void onReconnect(ReconnectedEvent e) {
+		e.getJDA().getPresence().setGame(Config.getGame());
+	}
 
 	@Override
 	public void onResume(ResumedEvent e) {
